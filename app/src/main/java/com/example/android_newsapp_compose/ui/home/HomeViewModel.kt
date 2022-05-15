6b59f96b6ceec6c5c5ad590ba4311d01377b7e21
oktayagca <okty_agca@hotmail.com>
2022-05-15 19:01:24 +0300

    Api key moved to gradle properties

--- app/src/main/java/com/example/android_newsapp_compose/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android_newsapp_compose/ui/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.example.android_newsapp_compose.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.android_newsapp_compose.BuildConfig
 import com.example.android_newsapp_compose.data.entities.TopNewsResponse
 import com.example.android_newsapp_compose.data.repository.Repository
 import com.example.android_newsapp_compose.utils.Resource
@@ -16,7 +17,7 @@ class HomeViewModel @Inject constructor(
     lateinit var news : LiveData<Resource<TopNewsResponse>>
 
     init {
-        news =getTopNews("tr","841a49c5f3eb4b88bd6d29c84297894d")
+        news =getTopNews("tr",BuildConfig.API_KEY)
     }
     fun getTopNews(country:String,key:String) = repository.getTopNews(country,key)
 }
